facc749e5e40c402b9b7dfdf434d117c17356726
jacqueline-mossig <jacqueline.mossig@hs-furtwangen.de>
2019-05-22 16:21:22 +0200

    Aufgabe 7

---
+++ TypeScript-Aufgabe/script.js
@@ -0,0 +1,8 @@
+console.log("Test1");
+//#sourceMappingURL=script.js.map
+document.getElementByID("Button").addEventListener("click", DoStuff);
+function DoStuff() {
+    console.log();
+    document.getElementById("Button").innerHTML = "Fick dich";
+}
+//# sourceMappingURL=script.js.map
---
+++ TypeScript-Aufgabe/script.ts
@@ -0,0 +1,15 @@
+console.log("Test1");
+//#sourceMappingURL=script.js.map
+
+document.getElementByID("Button").addEventListener("click", DoStuff)
+function DoStuff(){
+    console.log();
+    document.getElementById("Button").innerHTML="Fick dich"
+}
+
+
+
+
+
+
+
